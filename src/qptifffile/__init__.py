19da412d41375446adbda5ab2741cbab3de9ae94
grenkoca <grenkoca@gmail.com>
2026-02-26 09:22:56 -0600

    feat: [US-003] - Implement config loader (format_config.py)

--- src/qptifffile/__init__.py
+++ src/qptifffile/__init__.py
@@ -1,4 +1,5 @@
 from .qptifffile import QPTiffFile
 from .exceptions import MxTiffFormatError
+from .format_config import load_formats
 
-__all__ = ['QPTiffFile', 'MxTiffFormatError']
+__all__ = ['QPTiffFile', 'MxTiffFormatError', 'load_formats']
