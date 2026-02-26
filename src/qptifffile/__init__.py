f28a7df6427f81de8fb54e2b6bd5ee1901f8c865
grenkoca <grenkoca@gmail.com>
2026-02-26 09:30:38 -0600

    feat: [US-007] - Rename class to MxTiffFile with QPTiffFile alias

--- src/qptifffile/__init__.py
+++ src/qptifffile/__init__.py
@@ -1,7 +1,30 @@
-from .qptifffile import QPTiffFile
+import warnings
+
+from .mxtifffile import MxTiffFile
 from .exceptions import MxTiffFormatError
 from .format_config import load_formats
 from .format_detector import detect_format
 from .heuristic import heuristic_detect, ANCHOR_MARKER
 
-__all__ = ['QPTiffFile', 'MxTiffFormatError', 'load_formats', 'detect_format', 'heuristic_detect', 'ANCHOR_MARKER']
+
+class QPTiffFile(MxTiffFile):
+    """Deprecated alias for MxTiffFile. Use MxTiffFile instead."""
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            "QPTiffFile is deprecated, use MxTiffFile",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        super().__init__(*args, **kwargs)
+
+
+__all__ = [
+    'MxTiffFile',
+    'QPTiffFile',
+    'MxTiffFormatError',
+    'load_formats',
+    'detect_format',
+    'heuristic_detect',
+    'ANCHOR_MARKER',
+]
